a179626a9237f0d38b27969f3dc58d3621f83468
pracodeep <133084025+pracodeep@users.noreply.github.com>
2024-03-01 10:36:06 +0530

    Add files via upload

--- src/SelectionSort.java
+++ src/SelectionSort.java
@@ -1,2 +1,34 @@
-public class SelectionSort {
+ public class SelectionSort   {
+    static void selectionsort(int [] arr) {
+        int n = arr.length;
+
+        for (int i = 0; i < n - 1; i++) { //i represent the current element
+
+            //find the minimum element in unsorted part of the array
+            int min_index=i;
+            for(int j=i+1;j<n;j++){
+                if(arr[j]<arr[min_index]){
+                    min_index=j;
+                }
+            }
+            //swap current element and min-element-> current element i will have correct element
+
+            //arr[i],arr[min-index]
+
+            int temp=arr[i];
+            arr[i]=arr[min_index];
+            arr[min_index]=temp;
+
+
+
+        }
+    }
+    public static void main(String[] args) {
+        int [] arr={7,4,5,1,2};
+        selectionsort(arr);
+        for(int i=0;i<arr.length;i++){
+            System.out.print(arr[i]+"");
+        }
+    }
+
 }
